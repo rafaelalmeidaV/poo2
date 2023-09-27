9d095d476251ffe7f92bdf0399d292e45a1640dd
Rafael Almeida V <104868359+rafaelalmeidaV@users.noreply.github.com>
2023-09-26 22:25:39 -0300

    init

---
+++ projetoAula26_09/src/main/java/com/mycompany/projetoaula26_09/ProjetoAula26_09.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.projetoaula26_09;
+
+/**
+ *
+ * @author rafap
+ */
+public class ProjetoAula26_09 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
